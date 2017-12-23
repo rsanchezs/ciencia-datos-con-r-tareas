73f7fc827b1d876fa77ed461440c2c5bb9490feb
Ubuntu <rsanchezs@RServer.pvpzxdzhzdlubfr2udpnczw1ff.xx.internal.cloudapp.net>
2017-12-23 09:07:21 +0000

    Deleted tarea 4

--- 00interfaz-rstudio/04View/view/mi_primer_script.R
+++
@@ -1,8 +0,0 @@
-# Cargamos libreria
-library(readr)
-
-# Importamos dataset `centros_acogica.csv`
-centros_acogida <- read_csv2("data/centros_acogida.csv")
-
-# Visualizamos el dataframe `centros_acogida`
-View(centros_acogida)
