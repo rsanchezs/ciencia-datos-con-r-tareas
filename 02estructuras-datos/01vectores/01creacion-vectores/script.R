5ad87afb6bf0f5696de9e7319540133400dcc584
Rubén Sánchez Sancho <rsanchezs@uoc.edu>
2018-01-06 10:13:52 +0100

    Task creación de vectores finished

---
+++ 02estructuras-datos/01vectores/01creacion-vectores/script.R
@@ -0,0 +1,7 @@
+vector_double <- c(1, 2.5, 4.5)
+
+vector_integer <- c(1L, 6L, 10L)
+
+vector_logical <- 
+  
+vector_character <- 
---
+++ 02estructuras-datos/01vectores/01creacion-vectores/script_sol.R
@@ -0,0 +1,7 @@
+vector_double <- c(1, 2.5, 4.5)
+
+vector_integer <- c(1L, 6L, 10L)
+
+vector_logical <- c(TRUE, FALSE, TRUE)
+  
+vector_character <- c("Ciencia de Datos con R")
