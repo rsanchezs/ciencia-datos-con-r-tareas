89da3b047815760d06a8e2f527befc342acf726e
Rubén Sánchez Sancho <rsanchezs@uoc.edu>
2018-01-10 15:33:49 +0100

    Changed path to custom.css

--- 00sintaxis/03datos-basicos/.init.R
+++ 00sintaxis/03datos-basicos/.init.R
@@ -3,6 +3,9 @@ if(file.exists(".RData"))
   load(".RData")
 
 # Abre el script en el editor
-file.edit("script.R")
+if(file.exists("script.R")){
+  file.edit("script.R") 
+  message("Escribe check() y presiona la tecla Enter para ver la solucion de la tarea.\n")
+}
 
-message("2. Escribe check() y presiona la tecla Enter para ver la solucion de la tarea.\n")
+
