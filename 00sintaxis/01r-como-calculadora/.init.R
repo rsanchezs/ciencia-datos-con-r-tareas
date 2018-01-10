d5a7e3126f23ad01ecbf5c4b70952281ffbd4d9c
Rubén Sánchez Sancho <rsanchezs@uoc.edu>
2018-01-10 15:23:42 +0000

    Deleted .Rhistory and .init.R from the project directory

--- 00sintaxis/01r-como-calculadora/.init.R
+++
@@ -1,7 +0,0 @@
-# Carga .RData en en el Entorno Global
-if(file.exists(".RData"))
-  load(".RData")
-# Abre el script en el editor
-if(file.exists("script.R")){
-  file.edit("script.R") 
-}
--- 00sintaxis/02instruccion-asignacion/.init.R
+++
@@ -1,3 +0,0 @@
-# Carga .RData en en el Entorno Global
-if(file.exists(".RData"))
-  load(".RData")
