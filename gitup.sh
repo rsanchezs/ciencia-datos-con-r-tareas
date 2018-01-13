428ff0088dace601e87b406c8c75188172973852
Rubén Sánchez Sancho <rsanchezs@uoc.edu>
2018-01-13 13:35:02 +0000

    Initial commit en RStudio Server

--- gitup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while IFS='' read -r line || [[ -n "$repo" ]]; do
-    echo "Repo read from file repos.txt: $repo"
-    echo ""
-    echo "****** Getting latest for" ${repo} "******"
-    (cd "${repo}" && git checkout master && git up)
-    echo "******************************************"
-done < "$1"
