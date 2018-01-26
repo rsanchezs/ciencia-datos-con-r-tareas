f619b9b2c983d422a93153c99e629bd67f57e508
Rubén Sánchez Sancho <rsanchezs@uoc.edu>
2018-01-26 18:14:41 +0100

    Rprofile changed

---
+++ 01interfaz-rstudio/10pestaña-graficos/.init.R
@@ -0,0 +1,11 @@
+# Abre las instrucciones en el editor
+if (file.exists("script.R")){
+  file.edit("script.R")
+}
+
+# Creamos tres histogramas
+hist(rnorm(1000), col = "green")
+hist(runif(100), col = "red")
+hist(rexp(1000), col = "blue")
+
+
